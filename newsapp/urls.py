d9bc5051b01fffabb6c7d030e4edcea40542cc0a
unknown <kelvimanavadaria17012003@gmail.com>
2022-01-09 00:05:47 +0530

    Making some changes

--- newsapp/urls.py
+++ newsapp/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 
 urlpatterns = [
-    path('',views.home , name='home')
-    
+    path('',views.home , name='home'),
+    path('category/<str:k>',views.category,name = 'category'),
+    path('/language/<str:k>',views.language,name = 'language'),
+    path('/country/<str:k>',views.category,name = 'country')
 ]
--- newsapp/views.py
+++ newsapp/views.py
@@ -4,9 +4,47 @@ from newsapp import urls
 import requests
 # Create your views here.
 API_KEY = 'a85399a7f7c742d286ffb8ee88e47d48'
+categories = ['business','entertainment' ,'general','health' ,'science' ,'sports' ,'technology']
 
-def home(request):
-    url = f'https://newsapi.org/v2/everything?q=keyword&apiKey={API_KEY}'
+def home(request ):
+    
+    url = ('https://newsapi.org/v2/everything?'
+       'q=Apple&'
+       'from=2022-01-08&'
+       'sortBy=popularity&'
+       'apiKey=a85399a7f7c742d286ffb8ee88e47d48')
+
+    
+
+    response = requests.get(url)
+    data = response.json()
+    attribute = data['articles']
+    totalResults=data['totalResults']
+    context = {
+        'attribute' : attribute,
+        'totalResults':totalResults,
+        'categories':categories
+    }
+    return render(request,'newsapp/home.html',context)
+
+def category(request, k):
+    url = f'https://newsapi.org/v2/top-headlines/sources?category={k}apiKey={API_KEY}'
+    print(url)
+    response = requests.get(url)
+    data = response.json()
+    attribute = data['sources']
+    totalResults=data['totalResults']
+    context = {
+         
+        'attribute' : attribute,
+        'totalResults':totalResults
+    }
+    return render(request,'newsapp/home.html',context)
+
+
+def language(request, k):
+    url = f'https://newsapi.org/v2/top-headlines/sources?category={k}apiKey={API_KEY}'
+    print(url)
     response = requests.get(url)
     data = response.json()
     attribute = data['articles']
@@ -16,3 +54,5 @@ def home(request):
         'totalResults':totalResults
     }
     return render(request,'newsapp/home.html',context)
+
+
